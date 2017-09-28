9f1da975f09c500afb95822197f9375085d02207
MJRLegends <MJRLegends@users.noreply.github.com>
2017-09-28 15:22:45 +0100

    Added function to remove vanilla tools effectiveness

--- src/main/java/com/mjr/spaceAstronomyTweaks/MainEventHandler.java
+++ src/main/java/com/mjr/spaceAstronomyTweaks/MainEventHandler.java
@@ -1,11 +1,20 @@
 package com.mjr.spaceAstronomyTweaks;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntitySilverfish;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
@@ -61,4 +70,40 @@ public class MainEventHandler {
 			event.getEntity().setDead();
 		}
 	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onDamageTaken(LivingHurtEvent event) {
+		Entity entity = event.getSource().getEntity();
+		if (!(entity instanceof FakePlayer) && (entity instanceof EntityPlayer)) {
+			EntityPlayer player = (EntityPlayer) entity;
+
+			ItemStack stack = player.getHeldItemMainhand();
+			if (stack.getItem() instanceof ItemSword) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onItemUse(PlayerEvent.BreakSpeed event) {
+		EntityPlayer player = event.getEntityPlayer();
+		if (player != null) {
+			ItemStack stack = player.getHeldItemMainhand();
+			if (stack.getItem() instanceof ItemTool) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onHoeing(UseHoeEvent event) {
+		EntityPlayer player = event.getEntityPlayer();
+		if (player != null) {
+			ItemStack stack = player.getHeldItemMainhand();
+			if (stack.getItem() instanceof ItemHoe) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
 }
