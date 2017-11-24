5b97cec89ebf8baff5a3570ba5768318abf7fbd8
MJRLegends <MJRLegends@users.noreply.github.com>
2017-11-24 17:30:43 +0000

    Added option to Disable RefinedStorage External Storage with ColossalChests Interfaces Compatibility

--- src/main/java/com/mjr/spaceAstronomyTweaks/Config.java
+++ src/main/java/com/mjr/spaceAstronomyTweaks/Config.java
@@ -20,20 +20,20 @@ public class Config {
 	public static int quarryDim;
 	public static boolean removeToolEffectiveness;
 	public static boolean generateBOPEndOres;
+	public static boolean removeRF_CC_Compatibility;
 
 	public static void load() {
 		Configuration config = new Configuration(new File("config/SpaceAstronomyTweaks.cfg"));
 		config.load();
 
-		Config.autoServerList = config.get(Config.mainoptions, "Enable auto add servers to the list", true, "Setting this to false will stop servers being added and removed from your server list!").getBoolean(true);
-		Config.autoserverlistLink = config.get(Config.mainoptions, "Servers to the list url", "https://pastebin.com/raw/Y3FtGkf9", "Link the Auto server list management will use!").getString();
-		Config.clearSimpleAchievements = config.get(Config.mainoptions, "Clear Simple Achievements on Every World", false, "DONT TOUCH! OR IT WILL BREAK THE PACK").getBoolean(false);
-		Config.clearSimpleAchievementsWorlds = config.get("Worlds", Config.worlds, Config.clearSimpleAchievementsWorlds, "").getStringList();	
-		// defaultOptionsFile = config.get(mainoptions, "Use default bosses for all planets", false, "Will disable all custom bosses and will replace them with Creeper Bosses!").getBoolean(false);
-		Config.quarryDim = config.get(Config.mainoptions, "ExtraUtils2_Quarry_Dim", -9999, "Set this to the same as the ExtraUtils2_Quarry_Dim in the ExtraUtil 2 config").getInt(-9999);
-		Config.removeToolEffectiveness = config.get(Config.mainoptions, "Remove Effectiveness from Vanilla Tools", true, "").getBoolean(true);
-		Config.generateBOPEndOres = config.get(Config.mainoptions, "Enable fix for generating Biomes o Penty End Ores", true, "").getBoolean(true);
-
+		Config.autoServerList = config.get(Config.mainoptions, "Enable auto add servers to the list", true, "Setting this to false will stop servers being added and removed from your server list!. DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getBoolean(true);
+		Config.autoserverlistLink = config.get(Config.mainoptions, "Servers to the list url", "https://pastebin.com/raw/Y3FtGkf9", "Link the Auto server list management will use!. DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getString();
+		Config.clearSimpleAchievements = config.get(Config.mainoptions, "Clear Simple Achievements on Every World", false, "DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getBoolean(false);
+		Config.clearSimpleAchievementsWorlds = config.get("Worlds", Config.worlds, Config.clearSimpleAchievementsWorlds, "DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getStringList();	
+		Config.quarryDim = config.get(Config.mainoptions, "ExtraUtils2_Quarry_Dim", -9999, "Set this to the same as the ExtraUtils2_Quarry_Dim in the ExtraUtil 2 config. DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getInt(-9999);
+		Config.removeToolEffectiveness = config.get(Config.mainoptions, "Remove Effectiveness from Vanilla Tools", true, "DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getBoolean(true);
+		Config.generateBOPEndOres = config.get(Config.mainoptions, "Enable fix for generating Biomes o Penty End Ores", true, "DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getBoolean(true);
+		Config.removeRF_CC_Compatibility = config.get(Config.mainoptions, "Disable RefinedStorage External Storage with ColossalChests Interfaces Compatibility", true, "DONT TOUCH! OR IT WILL BREAK THE PACK, TO BE CHANGED BY THE MOD PACK DEV ONLY!").getBoolean(true);
 		config.save();
 	}
 
--- src/main/java/com/mjr/spaceAstronomyTweaks/MainEventHandler.java
+++ src/main/java/com/mjr/spaceAstronomyTweaks/MainEventHandler.java
@@ -1,18 +1,23 @@
 package com.mjr.spaceAstronomyTweaks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntitySilverfish;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
@@ -134,6 +139,74 @@ public class MainEventHandler {
 				return;
 			if (stack.getItem() == Items.WOODEN_HOE || stack.getItem() == Items.STONE_HOE || stack.getItem() == Items.IRON_HOE || stack.getItem() == Items.GOLDEN_HOE || stack.getItem() == Items.DIAMOND_HOE) {
 				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
+		EntityPlayer player = event.getPlayer();
+		World world = event.getWorld();
+		if (player != null && world != null) {
+			if (Config.removeToolEffectiveness == false)
+				return;
+			if (world.isRemote == false) {
+				Block placedBlock = event.getPlacedBlock().getBlock();
+				if (event.getPlacedBlock() != null) {
+					boolean isRSBlock = false;
+					boolean isCCBlock = false;
+
+					if (placedBlock.getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage"))
+						isRSBlock = true;
+					if (placedBlock.getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests"))
+						isCCBlock = true;
+
+					if (isRSBlock || isCCBlock) {
+						if (placedBlock.getRegistryName().getResourcePath().equalsIgnoreCase("external_storage") || placedBlock.getRegistryName().getResourcePath().equalsIgnoreCase("interface")) {
+							BlockPos posOld = event.getPos();
+							BlockPos pos = posOld;
+
+							pos = posOld.west();
+							IBlockState state = world.getBlockState(pos);
+							pos = posOld.south();
+							IBlockState state1 = world.getBlockState(pos);
+							pos = posOld.north();
+							IBlockState state2 = world.getBlockState(pos);
+							pos = posOld.east();
+							IBlockState state3 = world.getBlockState(pos);
+							pos = posOld.up();
+							IBlockState state4 = world.getBlockState(pos);
+							pos = posOld.down();
+							IBlockState state5 = world.getBlockState(pos);
+
+							if (state != null && state1 != null && state2 != null && state3 != null && state4 != null && state5 != null) {
+								boolean removeBlock = false;
+								if (isCCBlock) {
+									if (state.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage") || state1.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage")
+											|| state2.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage") || state3.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage")
+											|| state4.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage") || state5.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("refinedstorage"))
+										if (state.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage") || state1.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage")
+												|| state2.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage") || state3.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage")
+												|| state4.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage") || state5.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("external_storage"))
+											removeBlock = true;
+								} else if (isRSBlock) {
+									if (state.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests") || state1.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests")
+											|| state2.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests") || state3.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests")
+											|| state4.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests") || state5.getBlock().getRegistryName().getResourceDomain().equalsIgnoreCase("colossalchests"))
+										if (state.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface") || state1.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface")
+												|| state2.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface") || state3.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface")
+												|| state4.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface") || state5.getBlock().getRegistryName().getResourcePath().equalsIgnoreCase("interface"))
+											removeBlock = true;
+								}
+								if (removeBlock) {
+									if (!(player instanceof FakePlayer))
+										player.addChatMessage(new TextComponentString(TextFormatting.RED + "Sorry but this compatibility has been disabled due to issues!"));
+									event.setCanceled(true);
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
