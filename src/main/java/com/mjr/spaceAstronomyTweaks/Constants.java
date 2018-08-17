fcb07965474fd3401ebeadc434946a32c2613e32
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-17 12:20:14 +0100

    Released v1.1, Added Jar signing, Tweaked a few output messages, Fixed mod id needing to be lowercase

--- src/main/java/com/mjr/spaceAstronomyTweaks/Constants.java
+++ src/main/java/com/mjr/spaceAstronomyTweaks/Constants.java
@@ -3,9 +3,11 @@ package com.mjr.spaceAstronomyTweaks;
 public class Constants {
 	public static final String modID = "SpaceAstronomyTweaks";
 	public static final String modName = "SpaceAstronomyTweaks";
-	public static final String modVersion = "1.0";
+	public static final String modVersion = "1.1";
 
 	public static final String ASSET_PREFIX = "spaceAstronomyTweaks";
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
 	public static final String PREFIX = "spaceAstronomyTweaks.";
+	
+	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
 }
