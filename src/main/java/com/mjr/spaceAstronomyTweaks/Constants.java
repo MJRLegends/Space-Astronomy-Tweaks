fb462dbd5ffa231fd343c732a43fdb8eb9d93dcc
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-17 12:30:27 +0100

    Few final corrections for Release v0.3

--- src/main/java/com/mjr/spaceAstronomyTweaks/Constants.java
+++ src/main/java/com/mjr/spaceAstronomyTweaks/Constants.java
@@ -3,7 +3,7 @@ package com.mjr.spaceAstronomyTweaks;
 public class Constants {
 	public static final String modID = "spaceastronomytweaks";
 	public static final String modName = "SpaceAstronomyTweaks";
-	public static final String modVersion = "0.3";
+	public static final String modVersion = "1.11.2-0.3";
 
 	public static final String ASSET_PREFIX = modID;
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
